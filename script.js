5fe40b4af6ddcc853f70d3d466b7851238661db8
Giioz <mamaladzegio99@gmail.com>
2024-03-18 21:35:52 +0400

    wip (woking on deleting from localstorage)

---
+++ script.js
@@ -0,0 +1,60 @@
+let inp = document.getElementById('input')
+let addBtn = document.getElementById('btn')
+let ul = document.getElementById('ul')
+let values = localStorage.getItem('values') ? JSON.parse(localStorage.getItem('values')) : []
+
+
+addBtn.addEventListener('click', function(){
+    if(inp.value.trim() != ''){
+        let li = document.createElement('li');
+        ul.appendChild(li);
+        li.innerHTML = `<input type="checkbox" onclick="checkbox(event)"><span>${inp.value}</span><button id="btn1" onClick="deleteLi(event)">X</button>`;
+        values.push(`${inp.value}`)
+        localStorage.setItem('values', JSON.stringify(values));
+        location.reload()
+        inp.value = ""
+    }
+})
+
+function displayNotes(){
+    values.forEach(item => {
+        let li = document.createElement('li');
+        ul.appendChild(li);
+        li.innerHTML = `<input type="checkbox" onclick="checkbox(event)"><span>${item}</span><button id="btn1" class="delBtn" onClick="deleteLi(event)">X</button>`;
+    })
+}
+
+
+// in progress
+
+// function activateDeleteListeners() {
+//     let delBtn = document.querySelectorAll(".delBtn")
+//     delBtn.forEach((db, i) => {
+//         db.addEventListener('click', () => {deleteLi(i)})
+//     })
+// }
+
+// function deleteLi(i){
+//     values.splice(i+1, 1)
+//     localStorage.setItem('values', JSON.stringify(values))
+//     location.reload
+//     console.log(values);
+// }
+
+function deleteLi(e) {
+   e.target.parentNode.remove()
+   
+}
+
+
+function checkbox(e){
+    e.target.nextSibling.classList.toggle('line')
+}
+
+
+
+
+
+window.onload = function(){
+    displayNotes()
+}
